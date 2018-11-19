2156f49b6d6927d18a7693ff922c05319942232e
René Schultz Madsen <rm@microting.com>
2018-11-19 08:19:44 +0100

    Trying with a different replace.

--- src/app/water/water.component.ts
+++ src/app/water/water.component.ts
@@ -145,8 +145,11 @@ export class WaterComponent implements OnInit, AfterViewInit {
     // water - message
     let txtVal = this.currentMessage;
     console.log('currentMessage to be inserted is ' + JSON.stringify(this.currentMessage));
-    txtVal = txtVal.replace(/\r/g, '<br>');
-    txtVal = txtVal.replace(/\n/g, '<br>');
+    txtVal = txtVal.replace('<div>', '');
+    txtVal = txtVal.replace('</div>', '');
+    console.log('currentMessage to be inserted is after replace ' + JSON.stringify(this.currentMessage));
+    // txtVal = txtVal.replace(/\r/g, '<br>');
+    // txtVal = txtVal.replace(/\n/g, '<br>');
     txt_body = txt_body + 'F3# ' + txtVal;
 
     const item = Office.context.mailbox.item;
